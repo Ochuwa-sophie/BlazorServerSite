05706f9458c6053032cdc334b57c31e8ad653460
Ochuwa-sophie <sophiecodes@gmail.com>
2021-08-20 11:33:41 +0100

    upload project

---
+++ src/BlazorApp/Data/Form.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BlazorApp.Data
+{
+    public class Form
+    {
+        public string Title { get; set; }
+
+        public int Slug { get; set; }
+        public string Content { get; set; }
+    }
+}
